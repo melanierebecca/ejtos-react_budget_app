3c86194e8af21e8aa4b664de4dba6712eea7188f
melanierebecca <melaniewijesooriya@gmail.com>
2023-07-09 20:10:17 +0400

    add budget validation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,23 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
+    const { budget, dispatch, expenses } = useContext(AppContext);
     const setBudget = (value) => {
-        dispatch({
-            type: 'SET_BUDGET',
-            payload: value
-        });
+        if (value > 200000) alert("Budget cannot be greater than 200000");
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+        if (totalExpenses < value) {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: value
+            });
+        } else {
+            alert("You cannot reduce the budget lower than spending");
+        }
+
     }
     return (
         <div className='alert alert-secondary'>
             <span>Budget: <input
-                        type='number'
-                        id='budget'
-                        value={budget}
-                        max={200000}
-                        step="10"
-                        style={{ marginLeft: '0.5rem', size: 10 }}
-                        onChange={(event) => setBudget(event.target.value)}>
+                type='number'
+                id='budget'
+                value={budget}
+                max="200000"
+                step="10"
+                style={{ marginLeft: '0.5rem', size: 10 }}
+                onChange={(event) => setBudget(event.target.value)}>
             </input></span>
         </div>
     );
