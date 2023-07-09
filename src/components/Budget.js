8ce7ca8acab634b4d4bdbd47aa078d768a11ac9d
melanierebecca <melaniewijesooriya@gmail.com>
2023-07-09 19:27:20 +0400

    add budget input

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,24 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget, currency } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const setBudget = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        });
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: {currency}{budget}</span>
+            <span>Budget: <input
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        max={200000}
+                        step="10"
+                        style={{ marginLeft: '0.5rem', size: 10 }}
+                        onChange={(event) => setBudget(event.target.value)}>
+            </input></span>
         </div>
     );
 };
