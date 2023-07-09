e9d9957ca975e9fb9ccf2f02ca5e76c2cd22cad3
melanierebecca <melaniewijesooriya@gmail.com>
2023-07-09 16:56:48 +0400

    add enhanced app changes

--- src/App.js
+++ src/App.js
@@ -13,35 +13,36 @@ import ExpenseList from './components/ExpenseList';
 import ExpenseTotal from './components/ExpenseTotal';
 
 import { AppProvider } from './context/AppContext';
+import CurrencyDropDown from './components/CurrencyDropdown';
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */} 
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
-                    </div>      
-
-                        {/* Add Remaining component here under */}        
+                    </div>
                     <div className='col-sm'>
                         <Remaining />
                     </div>
-                        {/* Add ExpenseTotal component here under */}        
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
-                        {/* Add ExpenseList component here under */}         
+                    <div className='col-sm'>
+                        <CurrencyDropDown />
+                    </div>
+                </div>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
-                        {/* Add ExpenseItem component here under */}        
                     <div className='col-sm'>
                         <ExpenseItem />
                     </div>
-                        {/* Add AllocationForm component here under */}        
+                </div>
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm />
                     </div>
---
+++ src/components/CurrencyDropdown.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const CurrencyDropDown = () => {
+    const { dispatch, currency } = useContext(AppContext);
+
+    const setCurrency = (value) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: value
+        });
+    }
+    return (
+        <div className='alert alert-success'>
+                <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setCurrency(event.target.value)}>
+                    <option value="$" name="Dollar">$ Dollar</option>
+                    <option value="£" name="Pound">£ Pound</option>
+                    <option value="€" name="Euro">€ Euro</option>
+                    <option value="₹" name="Ruppee">₹ Ruppee</option>
+                </select>
+
+        </div>
+    );
+};
+export default CurrencyDropDown;
